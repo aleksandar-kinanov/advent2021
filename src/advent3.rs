cb1b386d2d63f950d4d78a734eb25fbf957c3d43
Aleksandar Kinanov <alex-cool-mail@no-company.com>
2022-11-18 11:33:20 +0200

    Update tasks

--- src/advent3.rs
+++ src/advent3.rs
@@ -4,26 +4,27 @@ use std::str::FromStr;
 
 #[derive(Debug)]
 struct Position {
-    horizontal_distance: u32,
-    depth: u32,
-    aim: u32
+    x: i32,
+    y: i32
 }
 
 enum Movement {
     Forward,
     Down,
-    Up
+    Up,
+    Backward
 }
 
 impl Position {
     fn new() -> Position {
-        Position { horizontal_distance: 0, depth: 0, aim: 0}
+        Position { x: 0, y: 0}
     }
-    fn mv(&mut self, direction: Movement, distance: u32){
+    fn mv(&mut self, direction: Movement, distance: i32){
         match direction {
-            Movement::Forward => {self.horizontal_distance += distance; self.depth = self.depth + (distance * self.aim)},
-            Movement::Down => {self.aim += distance},
-            Movement::Up => {self.aim -= distance},
+            Movement::Forward => {self.x += distance},
+            Movement::Backward => {self.x -= distance},
+            Movement::Down => {self.y += distance},
+            Movement::Up => {self.y -= distance},
         };
     }
 }
@@ -35,6 +36,7 @@ impl FromStr for Movement {
     fn from_str(input: &str) -> Result<Movement, Self::Err> {
         match input {
             "forward"  => Ok(Movement::Forward),
+            "backward"  => Ok(Movement::Backward),
             "up"  => Ok(Movement::Up),
             "down" => Ok(Movement::Down),
             _      => Err(()),
@@ -47,11 +49,10 @@ fn main (){
     let data = fs::read_to_string("./dummy_data.txt").expect("Unable to read file");
     for item in data.split('\n'){
         match item.split_once(' '){
-            Some((distance, direction)) => { obj.mv(Movement::from_str(distance).unwrap(), direction.parse::<u32>().unwrap())},
+            Some((distance, direction)) => { obj.mv(Movement::from_str(distance).unwrap(), direction.parse::<i32>().unwrap())},
             _ => ()
         }
     }
-    println!("{} - {}", obj.horizontal_distance, obj.depth);
-    let result = obj.horizontal_distance * obj.depth;
+    let result = obj.x * obj.y;
     println!("{:#?}", result)
 }
