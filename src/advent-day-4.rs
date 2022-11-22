27edbfd4d76af10c0d7ab16ffb8f60076a61411b
Aleksandar Kinanov <akinanov@vmware.com>
2022-11-22 21:43:23 +0200

    Add comments

--- src/advent-day-4.rs
+++ src/advent-day-4.rs
@@ -32,7 +32,9 @@ fn main() {
     let mut matrices: Vec<Matrix> = Vec::new();
     let mut winners: Vec<Matrix> = Vec::new();
     let data = fs::read_to_string("./dummy_data.txt").expect("Unable to read file");
+    // Remove empty lines
     let mut parsed_data: Vec<&str> = data.split("\n").filter(|x| !x.is_empty()).collect();
+    // Get 0th element of the loaded data and load as ints
     let numbers: Vec<i8> = parsed_data.clone()[0]
         .split(",")
         .map(|x| x.parse::<i8>().unwrap())
@@ -41,6 +43,7 @@ fn main() {
     parsed_data.remove(0);
     let mut current = Matrix::new();
 
+    // Iterate over every row and remove all unnecessary empty spaces and new lines and replace them with ','
     for row in parsed_data {
         let parsed_row: Vec<i32> = row
             .replace(" ", ",")
@@ -50,8 +53,9 @@ fn main() {
             .map(|x| x.trim_start().parse::<i32>().unwrap())
             .collect();
         
+        // Append the parsed arrays 1 by 1 to the Matrix object
         current.append(parsed_row);
-
+        // Stop adding elements once the matrix is 5x5
         if current.is_full() {
             matrices.push(current.clone());
             current = Matrix::new();
@@ -65,9 +69,11 @@ fn main() {
             if matrix.is_winner {
                 continue
             }
+            // Replace every matching element with 100 as the matices have upper bound of elements of 99
             matrix.array.map_inplace(|x| {
                 if x == &(number as i32) {
                     *x = 100;
+                    // Add incremented values so they can be removed from the total sum of the matrix
                     matrix.increment += 100;
                 }
             });
@@ -79,6 +85,8 @@ fn main() {
         }
     }
     let last_winner = winners.last().unwrap();
+    // Get the Matrix that was first in getting a 'bingo' - entire column/row of 100s
     println!("First winner: {:?}", (winners[0].array.sum() - winners[0].increment) * (winners[0].winning_number as i32));
+    // Get the Matrix that was last in getting a 'bingo' - entire column/row of 100s
     println!("Last Winner: {:?}", (last_winner.array.sum() - last_winner.increment) * (last_winner.winning_number as i32));
 }
